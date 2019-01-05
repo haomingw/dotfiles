b11504b35896d9ec741d65383063a67681bb4842
Haoming Wang <haoming.exe@gmail.com>
2019-01-06 00:13:12 +0100

    zsh dotfiles (#1)
    * zsh dotfiles
    
    * fix absolute home path

--- install.sh
+++ install.sh
@@ -1,23 +1,64 @@
 #!/bin/bash
-
 #   Copyright 2018 Haoming Wang
+set -e
 
 app_name='xming-dotfiles'
 [ -z "$APP_PATH" ] && APP_PATH="$(pwd)"
+[ -z "$ZSH_CUSTOM" ] && ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 debug_mode='0'
+options=("vim" "oh-my-zsh" "zsh-plugins")
 
 . ./utils.sh
 
-do_backup   	"$HOME/.vim" \
-                "$HOME/.vimrc" \
-                "$HOME/.gvimrc"
+install_vim() {
+    program_must_exist "vim"
+    program_must_exist "git"
 
-create_symlinks "$APP_PATH" \
-                "$HOME"
+    do_backup       "$HOME/.vim" \
+                    "$HOME/.vimrc" \
+                    "$HOME/.gvimrc"
 
-setup_vim_plug
+    create_symlinks "$APP_PATH" \
+                    "$HOME"
 
-post_install_vim
+    setup_vim_plug
+
+    post_install_vim
+}
+
+install_zsh() {
+    program_must_exist "zsh"
+    program_must_exist "git"
+    program_must_exist "curl"
+
+    install_oh_my_zsh
+}
+
+config_zsh() {
+    program_must_exist "zsh"
+
+    install_zsh_plugins "$ZSH_CUSTOM/plugins"
+    setup_zsh           "$APP_PATH/zsh" \
+                        "$HOME"
+}
+
+confirm() {
+    read -p "Do you want to continue? (y/N) " choice
+    case $choice in
+        [yY][eE][sS]|[yY] ) ;;
+        * ) exit 0;;
+    esac
+}
+
+PS3='Please enter your choice: '
+select opt in "${options[@]}"; do
+    case $opt in
+        "vim") install_vim;;
+        "oh-my-zsh") install_zsh;;
+        "zsh-plugins") config_zsh;;
+    esac
+    confirm
+done
 
 msg             "\nThanks for installing $app_name."
 msg             "© `date +%Y` http://flyingmouse.github.io/"
