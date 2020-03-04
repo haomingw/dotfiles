140bf4ee1b3defc8a38b95aba9b7ecf1a3db477d
Haoming Wang <haoming.exe@gmail.com>
2020-03-04 02:14:59 +0100

    change tab4 to tab2

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ APP_PATH="${APP_PATH:-$(pwd)}"
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
 options=(
-    "vim"
-    "oh-my-zsh"
-    "zsh-plugins"
-    "python"
-    "tmux"
-    "sublime-vscode"
+  "vim"
+  "oh-my-zsh"
+  "zsh-plugins"
+  "python"
+  "tmux"
+  "sublime-vscode"
 )
 
 zsh_plugins=(
-    "zsh-users/zsh-autosuggestions"
-    "zsh-users/zsh-syntax-highlighting"
-    "history-substring-search"
+  "zsh-users/zsh-autosuggestions"
+  "zsh-users/zsh-syntax-highlighting"
+  "history-substring-search"
 )
 
 source utils.sh
@@ -27,126 +27,126 @@ source utils.sh
 ############################ SETUP FUNCTIONS
 
 print_select_menu() {
-    local prev="$PS3"
-    PS3=""
-    echo toto | select foo in "${options[@]}"; do break; done  # dummy select
-    PS3=$prev
+  local prev="$PS3"
+  PS3=""
+  echo toto | select foo in "${options[@]}"; do break; done  # dummy select
+  PS3=$prev
 }
 
 ############################ MAIN FUNCTIONS
 
 install_or_update_vim() {
-    program_must_exist  "vim"
-    program_must_exist  "git"
+  program_must_exist  "vim"
+  program_must_exist  "git"
 
-    create_vim_symlinks "$APP_PATH"
+  create_vim_symlinks "$APP_PATH"
 
-    setup_neovim
+  setup_neovim
 
-    setup_vim_plug
+  setup_vim_plug
 }
 
 install_oh_my_zsh() {
-    program_must_exist  "zsh"
-    program_must_exist  "git"
-    program_must_exist  "curl"
+  program_must_exist  "zsh"
+  program_must_exist  "git"
+  program_must_exist  "curl"
 
-    [ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    success "oh my zsh installed"
+  [ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  success "oh my zsh installed"
 }
 
 install_zsh_plugins() {
-    program_must_exist  "zsh"
-    file_must_exist     "$HOME/.zshrc"
-    file_must_exist     "$HOME/.oh-my-zsh"
+  program_must_exist  "zsh"
+  file_must_exist   "$HOME/.zshrc"
+  file_must_exist   "$HOME/.oh-my-zsh"
 
-    config_zshrc        "$APP_PATH"
-    zsh_plug            $(reverse ${zsh_plugins[@]})
+  config_zshrc    "$APP_PATH"
+  zsh_plug      $(reverse ${zsh_plugins[@]})
 }
 
 config_python() {
-    rm -rf ~/.ptpython ~/.linter
-    lnif $APP_PATH/python/ptpython ~/.ptpython
-    lnif $APP_PATH/python/linter   ~/.linter
-    install_miniconda_if_not_exists
-    success "Now configuring python."
+  rm -rf ~/.ptpython ~/.linter
+  lnif $APP_PATH/python/ptpython ~/.ptpython
+  lnif $APP_PATH/python/linter   ~/.linter
+  install_miniconda_if_not_exists
+  success "Now configuring python."
 }
 
 config_tmux() {
-    program_must_exist  "tmux"
-    mkdir -p ~/.tmux/plugins
-    if [ ! -d ~/.tmux/plugins/tpm ]; then
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    fi
-    lnif $APP_PATH/tmux/tmux.conf $HOME/.tmux.conf
-    success "Now configuring tmux."
+  program_must_exist  "tmux"
+  mkdir -p ~/.tmux/plugins
+  if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  fi
+  lnif $APP_PATH/tmux/tmux.conf $HOME/.tmux.conf
+  success "Now configuring tmux."
 }
 
 config_sublime_vscode() {
-    program_exists "subl" && {
-        local sublime_home
-        local sublime_keymap
-        is_linux && {
-            sublime_home="$HOME/.config/sublime-text-3/Packages/User"
-            sublime_keymap="Default (Linux).sublime-keymap"
-        }
-        if [ ! -z $sublime_home ] && [ -d $sublime_home ]; then
-            for file in $APP_PATH/sublime/*.sublime-settings; do
-                lnif $file $sublime_home
-            done
-            lnif "$APP_PATH/sublime/$sublime_keymap" $sublime_home
-            success "Now configuring sublime-text."
-        fi
+  program_exists "subl" && {
+    local sublime_home
+    local sublime_keymap
+    is_linux && {
+      sublime_home="$HOME/.config/sublime-text-3/Packages/User"
+      sublime_keymap="Default (Linux).sublime-keymap"
     }
+    if [ ! -z $sublime_home ] && [ -d $sublime_home ]; then
+      for file in $APP_PATH/sublime/*.sublime-settings; do
+        lnif $file $sublime_home
+      done
+      lnif "$APP_PATH/sublime/$sublime_keymap" $sublime_home
+      success "Now configuring sublime-text."
+    fi
+  }
 
-    program_exists "code" && {
-        local code_home
-        is_linux && code_home="$HOME/.config/Code/User"
-        is_macos && code_home="$HOME/Library/Application Support/Code/User"
-        increase_watch_limit
-        watch_limit_is_increased && install_vscode_extensions
-        if [ ! -z "$code_home" ]; then
-            lnif $APP_PATH/vscode/settings.json    $code_home
-            lnif $APP_PATH/vscode/keybindings.json $code_home
-            rm -rf $code_home/snippets
-            lnif $APP_PATH/vscode/snippets         $code_home
-            success "Now configuring vscode."
-        fi
-    } || true
+  program_exists "code" && {
+    local code_home
+    is_linux && code_home="$HOME/.config/Code/User"
+    is_macos && code_home="$HOME/Library/Application Support/Code/User"
+    increase_watch_limit
+    watch_limit_is_increased && install_vscode_extensions
+    if [ ! -z "$code_home" ]; then
+      lnif $APP_PATH/vscode/settings.json  $code_home
+      lnif $APP_PATH/vscode/keybindings.json $code_home
+      rm -rf $code_home/snippets
+      lnif $APP_PATH/vscode/snippets     $code_home
+      success "Now configuring vscode."
+    fi
+  } || true
 }
 
 bye() {
-    msg "\nThanks for installing $app_name."
-    msg "© `date +%Y` http://flyingmouse.github.io/"
-    exit 0
+  msg "\nThanks for installing $app_name."
+  msg "© `date +%Y` http://flyingmouse.github.io/"
+  exit 0
 }
 
 config() {
-    case "$1" in
-        "vim")            install_or_update_vim ;;
-        "oh-my-zsh")      install_oh_my_zsh ;;
-        "zsh-plugins")    install_zsh_plugins ;;
-        "python")         config_python ;;
-        "tmux")           config_tmux ;;
-        "sublime-vscode") config_sublime_vscode ;;
-        *)                error "Unexpected option: $1" ;;
-    esac
+  case "$1" in
+    "vim")      install_or_update_vim ;;
+    "oh-my-zsh")    install_oh_my_zsh ;;
+    "zsh-plugins")  install_zsh_plugins ;;
+    "python")     config_python ;;
+    "tmux")       config_tmux ;;
+    "sublime-vscode") config_sublime_vscode ;;
+    *)        error "Unexpected option: $1" ;;
+  esac
 }
 
 repeat_config() {
-    PS3='Please enter your choice: '
-    select option in "${options[@]}"; do
-        config $option
-        confirm_finish
-    done
+  PS3='Please enter your choice: '
+  select option in "${options[@]}"; do
+    config $option
+    confirm_finish
+  done
 }
 
 confirm_finish() {
-    confirm "Do you want to continue?" && print_select_menu || bye
+  confirm "Do you want to continue?" && print_select_menu || bye
 }
 
 main() {
-    [ $# -eq 1 ] && config $1 || repeat_config "$@"
+  [ $# -eq 1 ] && config $1 || repeat_config "$@"
 }
 
 main "$@"
--- utils.sh
+++ utils.sh
@@ -1,230 +1,230 @@
 ############################  BASIC SETUP TOOLS
 
 for file in common/*; do
-    source $file
+  source $file
 done
 
 msg() {
-    printf '%b\n' "$1" >&2
+  printf '%b\n' "$1" >&2
 }
 
 program_must_exist() {
-    program_exists "$1" || {
-        error "You must have '$1' installed to continue."
-        exit 1
-    }
+  program_exists "$1" || {
+    error "You must have '$1' installed to continue."
+    exit 1
+  }
 }
 
 file_must_exist() {
-    local file_path=$1
+  local file_path=$1
 
-    if [ ! -f "$file_path" ] && [ ! -d "$file_path" ]; then
-        error "You must have '$1' to continue."
-        exit 1
-    fi
+  if [ ! -f "$file_path" ] && [ ! -d "$file_path" ]; then
+    error "You must have '$1' to continue."
+    exit 1
+  fi
 }
 
 git_clone_to() {
-    local git_url=$1
-    local target_path=$2
-    local repo_name=$(basename "$git_url" .git)
+  local git_url=$1
+  local target_path=$2
+  local repo_name=$(basename "$git_url" .git)
 
-    if [ -d "$target_path" ] && [ ! -d "$target_path/$repo_name" ]; then
-        cd $target_path && git clone $git_url
-    fi
+  if [ -d "$target_path" ] && [ ! -d "$target_path/$repo_name" ]; then
+    cd $target_path && git clone $git_url
+  fi
 }
 
 parse_filename() {
-    echo $1 | rev | cut -d'/' -f1 | rev
+  echo $1 | rev | cut -d'/' -f1 | rev
 }
 
 update_vim_plugins() {
-    if [ -z $TRAVIS ]; then
-        vim +PlugClean! +qall && vim +PlugUpdate +qall
-    else
-        vim +PlugClean! +qall >/dev/null 2>&1
-        vim +PlugUpdate +qall >/dev/null 2>&1
-    fi
+  if [ -z $TRAVIS ]; then
+    vim +PlugClean! +qall && vim +PlugUpdate +qall
+  else
+    vim +PlugClean! +qall >/dev/null 2>&1
+    vim +PlugUpdate +qall >/dev/null 2>&1
+  fi
 }
 
 ############################ SETUP FUNCTIONS
 
 do_backup() {
-    if [ -e "$1" ]; then
-        msg "Attempting to back up your original configuration."
-        today=`date +%Y%m%d_%s`
-        [ -e "$1" ] && [ ! -L "$1" ] && mv -v "$1" "$1.$today";
-        success "Your original configuration has been backed up."
+  if [ -e "$1" ]; then
+    msg "Attempting to back up your original configuration."
+    today=`date +%Y%m%d_%s`
+    [ -e "$1" ] && [ ! -L "$1" ] && mv -v "$1" "$1.$today";
+    success "Your original configuration has been backed up."
    fi
 }
 
 create_vim_symlinks() {
-    local app=$1
+  local app=$1
 
-    mkdir -p $HOME/.vim/undo
-    for file in $app/vim/vim/*; do
-        lnif $file "$HOME/.vim/$(parse $file)"
-    done
-    lnif "$app/vim/vimrc"                   "$HOME/.vimrc"
-    lnif "$app/vim/ideavimrc"               "$HOME/.ideavimrc"
+  mkdir -p $HOME/.vim/undo
+  for file in $app/vim/vim/*; do
+    lnif $file "$HOME/.vim/$(parse $file)"
+  done
+  lnif "$app/vim/vimrc"           "$HOME/.vimrc"
+  lnif "$app/vim/ideavimrc"         "$HOME/.ideavimrc"
 
-    lnif "$app/vim/vim/static/clang-format" "$HOME/.clang-format"
-    lnif "$app/vim/vim/static/style.yapf"   "$HOME/.style.yapf"
+  lnif "$app/vim/vim/static/clang-format" "$HOME/.clang-format"
+  lnif "$app/vim/vim/static/style.yapf"   "$HOME/.style.yapf"
 
-    success "Setting up vim symlinks."
+  success "Setting up vim symlinks."
 }
 
 setup_neovim() {
-    mkdir -p $HOME/.config
-    lnif "$HOME/.vim"         "$HOME/.config/nvim"
-    lnif "$HOME/.vimrc"       "$HOME/.config/nvim/init.vim"
-    success "Setting up neovim."
+  mkdir -p $HOME/.config
+  lnif "$HOME/.vim"     "$HOME/.config/nvim"
+  lnif "$HOME/.vimrc"     "$HOME/.config/nvim/init.vim"
+  success "Setting up neovim."
 }
 
 setup_vim_plug() {
-    local system_shell="$SHELL"
-    export SHELL='/bin/sh'
+  local system_shell="$SHELL"
+  export SHELL='/bin/sh'
 
-    update_vim_plugins
+  update_vim_plugins
 
-    export SHELL="$system_shell"
+  export SHELL="$system_shell"
 
-    success "Now updating/installing plugins using vim-plug"
+  success "Now updating/installing plugins using vim-plug"
 }
 
 use_zsh_plugin() {
-    local pattern="^\s*$1$"
-    local target="$HOME/.zshrc"
-    sed -i "/$pattern/d" $target
-    grep -q $pattern $target || {
-        sed -i "/^plugins=/a \    $1" $target
-    }
+  local pattern="^\s*$1$"
+  local target="$HOME/.zshrc"
+  sed -i "/$pattern/d" $target
+  grep -q $pattern $target || {
+    sed -i "/^plugins=/a \  $1" $target
+  }
 }
 
 use_zsh_plugins() {
-    for plugin in "$@"; do
-        use_zsh_plugin $plugin
-    done
+  for plugin in "$@"; do
+    use_zsh_plugin $plugin
+  done
 }
 
 zsh_plug() {
-    local plugin_path="$ZSH_CUSTOM/plugins"
+  local plugin_path="$ZSH_CUSTOM/plugins"
 
-    for plugin in "$@"; do
-        if [[ $plugin == */* ]]; then
-            use_zsh_plugin $(parse $plugin)
-            if [ ! -d "$plugin_path/$plugin" ]; then
-                git_clone_to https://github.com/$plugin.git $plugin_path
-            fi
-        else
-            use_zsh_plugin $plugin
-        fi
-        msg "Now installing zsh plugin $plugin."
-    done
+  for plugin in "$@"; do
+    if [[ $plugin == */* ]]; then
+      use_zsh_plugin $(parse $plugin)
+      if [ ! -d "$plugin_path/$plugin" ]; then
+        git_clone_to https://github.com/$plugin.git $plugin_path
+      fi
+    else
+      use_zsh_plugin $plugin
+    fi
+    msg "Now installing zsh plugin $plugin."
+  done
 }
 
 append_if_not_exists() {
-    local file=$1
-    local content=$2
-    if [ ! -f $file ] || ! $(grep -q "$content" $file); then
-        echo $content >> $file
-    fi
+  local file=$1
+  local content=$2
+  if [ ! -f $file ] || ! $(grep -q "$content" $file); then
+    echo $content >> $file
+  fi
 }
 
 config_zshrc() {
-    local app_path=$1
-    local zshrc="$HOME/.zshrc"
-    sed -i '/plugins=(git)/c \plugins=(\n    git\n)' $zshrc
-    lnif $app_path/common                 $HOME/.common
-    for file in $app_path/zsh/*; do
-        lnif $file "$HOME/.$(parse $file)"
-    done
-    local cmd='[[ -s $HOME/.zshrc.local ]] && source $HOME/.zshrc.local'
-    append_if_not_exists $zshrc "$cmd"
+  local app_path=$1
+  local zshrc="$HOME/.zshrc"
+  sed -i '/plugins=(git)/c \plugins=(\n  git\n)' $zshrc
+  lnif $app_path/common         $HOME/.common
+  for file in $app_path/zsh/*; do
+    lnif $file "$HOME/.$(parse $file)"
+  done
+  local cmd='[[ -s $HOME/.zshrc.local ]] && source $HOME/.zshrc.local'
+  append_if_not_exists $zshrc "$cmd"
 
-    # set ibus for archlinux
-    program_exists pacman && {
-        cmd='ibus-daemon -drx'
-        append_if_not_exists $HOME/.profile "$cmd"
-    }
+  # set ibus for archlinux
+  program_exists pacman && {
+    cmd='ibus-daemon -drx'
+    append_if_not_exists $HOME/.profile "$cmd"
+  }
 
-    success "Now configuring zsh."
+  success "Now configuring zsh."
 }
 
 cleanup_miniconda_files() {
-    local installation_file=$1
-    rm $installation_file
-    find $HOME/miniconda3 \( -type f -o -type l \) \
-        -not -path "$HOME/miniconda3/pkgs*" \
-        -regex ".*bin/wish[0-9\.]*$" -ls -delete
-    success "Cleaning up minconda files"
+  local installation_file=$1
+  rm $installation_file
+  find $HOME/miniconda3 \( -type f -o -type l \) \
+    -not -path "$HOME/miniconda3/pkgs*" \
+    -regex ".*bin/wish[0-9\.]*$" -ls -delete
+  success "Cleaning up minconda files"
 }
 
 install_miniconda_if_not_exists() {
-    local conda="$HOME/miniconda3"
-    local pip_packages="$HOME/.pip_packages"
+  local conda="$HOME/miniconda3"
+  local pip_packages="$HOME/.pip_packages"
 
-    if [ ! -d $conda ]; then
-        local url
-        local conda_repo="https://repo.anaconda.com/miniconda"
-        is_linux && url="$conda_repo/Miniconda3-latest-Linux-x86_64.sh"
-        is_macos && url="$conda_repo/Miniconda3-latest-MacOSX-x86_64.sh"
-        if [ ! -z "$url" ]; then
-            local miniconda=$(parse_filename $url)
-            local target="$HOME/Downloads"
-            [ -f "$target/$miniconda" ] || wget $url -P $target
-            bash $target/$miniconda \
-            && success "Miniconda successfully installed" \
-            && cleanup_miniconda_files $target/$miniconda
-            success "Writing pip packages to $pip_packages"
-            $conda/bin/pip freeze > $pip_packages
-        fi
+  if [ ! -d $conda ]; then
+    local url
+    local conda_repo="https://repo.anaconda.com/miniconda"
+    is_linux && url="$conda_repo/Miniconda3-latest-Linux-x86_64.sh"
+    is_macos && url="$conda_repo/Miniconda3-latest-MacOSX-x86_64.sh"
+    if [ ! -z "$url" ]; then
+      local miniconda=$(parse_filename $url)
+      local target="$HOME/Downloads"
+      [ -f "$target/$miniconda" ] || wget $url -P $target
+      bash $target/$miniconda \
+      && success "Miniconda successfully installed" \
+      && cleanup_miniconda_files $target/$miniconda
+      success "Writing pip packages to $pip_packages"
+      $conda/bin/pip freeze > $pip_packages
     fi
+  fi
 }
 
 watch_limit_is_increased() {
-    program_exists sysctl && {
-        local limit=$(sysctl fs.inotify.max_user_watches | cut -d' ' -f3)
-        [ $limit -ge 524288 ]
-    }
+  program_exists sysctl && {
+    local limit=$(sysctl fs.inotify.max_user_watches | cut -d' ' -f3)
+    [ $limit -ge 524288 ]
+  }
 }
 
 increase_watch_limit() {
-    # if the limit has been increased, do nothing
-    watch_limit_is_increased && return 0
+  # if the limit has been increased, do nothing
+  watch_limit_is_increased && return 0
 
-    local watch_limit="fs.inotify.max_user_watches=524288"
-    local cfg
-    program_exists apt && cfg="/etc/sysctl.conf"
-    program_exists pacman && cfg="/etc/sysctl.d/50-max_user_watches.conf"
-    local message=(
-        "Do you want to increase (requires sudo password)"
-        "inotify limit? (y/N) "
-    )
-    if [ ! -z $cfg ] && [ -r $cfg ]; then
-        read -p "${message[*]}"
-        case $REPLY in
-            [yY][eE][sS]|[yY]) ;;
-            *) return 0 ;;
-        esac
-        cat $cfg | grep -q "$watch_limit" || {
-            echo $watch_limit | sudo tee -a $cfg >/dev/null
-        }
-        program_exists apt && sudo sysctl -p >/dev/null
-        program_exists pacman && sudo sysctl --system >/dev/null
-        success "Increasing inotify watcher limit"
-    fi
+  local watch_limit="fs.inotify.max_user_watches=524288"
+  local cfg
+  program_exists apt && cfg="/etc/sysctl.conf"
+  program_exists pacman && cfg="/etc/sysctl.d/50-max_user_watches.conf"
+  local message=(
+    "Do you want to increase (requires sudo password)"
+    "inotify limit? (y/N) "
+  )
+  if [ ! -z $cfg ] && [ -r $cfg ]; then
+    read -p "${message[*]}"
+    case $REPLY in
+      [yY][eE][sS]|[yY]) ;;
+      *) return 0 ;;
+    esac
+    cat $cfg | grep -q "$watch_limit" || {
+      echo $watch_limit | sudo tee -a $cfg >/dev/null
+    }
+    program_exists apt && sudo sysctl -p >/dev/null
+    program_exists pacman && sudo sysctl --system >/dev/null
+    success "Increasing inotify watcher limit"
+  fi
 }
 
 install_vscode_extensions() {
-    extensions=(
-        "ms-vscode.go"
-        "eamodio.gitlens"
-        "ms-python.python"
-        "mitaki28.vscode-clang"
-    )
-    for extension in "${extensions[@]}"; do
-        code --install-extension $extension
-    done
-    success "Vscode extensions are installed"
+  extensions=(
+    "ms-vscode.go"
+    "eamodio.gitlens"
+    "ms-python.python"
+    "mitaki28.vscode-clang"
+  )
+  for extension in "${extensions[@]}"; do
+    code --install-extension $extension
+  done
+  success "Vscode extensions are installed"
 }
