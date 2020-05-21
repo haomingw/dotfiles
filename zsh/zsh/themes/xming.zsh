bc1ed8809bc168af189169a74b5bfd020675ecfc
Haoming Wang <haoming.exe@gmail.com>
2020-05-21 18:33:49 +0200

    minimal zsh theme

--- zsh/zsh/themes/xming.zsh
+++ zsh/zsh/themes/xming.zsh
@@ -1,6 +1,6 @@
 PROMPT='%B%F{cyan}%c%f%b$(git_prompt_info) %(?.%F{green}.%F{red})❯%f '
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" %F{blue}git:(%F{red}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" %F{blue}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%f"
-ZSH_THEME_GIT_PROMPT_DIRTY="%F{yellow}*%F{blue})"
-ZSH_THEME_GIT_PROMPT_CLEAN="%F{blue})"
+ZSH_THEME_GIT_PROMPT_DIRTY="%F{yellow}*"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
