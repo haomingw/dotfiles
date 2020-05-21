02d854a133094de8464d7e745a8e5b5b49aed310
Haoming Wang <haoming.exe@gmail.com>
2020-05-22 01:09:34 +0200

    change git branch color

--- zsh/zsh/themes/xming.zsh
+++ zsh/zsh/themes/xming.zsh
@@ -1,6 +1,6 @@
 PROMPT='%B%F{cyan}%c%f%b$(git_prompt_info) %(?.%F{green}.%F{red})❯%f '
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" %F{blue}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" %F{242}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%f"
 ZSH_THEME_GIT_PROMPT_DIRTY="%F{yellow}*"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
