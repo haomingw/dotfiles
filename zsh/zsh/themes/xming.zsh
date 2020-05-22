0cc38b00e0be2b1be7a47601c23cfdcf9d80cdd9
Haoming Wang <haoming.exe@gmail.com>
2020-05-22 17:39:21 +0200

    zsh theme error handling

--- zsh/zsh/themes/xming.zsh
+++ zsh/zsh/themes/xming.zsh
@@ -27,6 +27,14 @@ prompt_refresh() {
 prompt_callback() {
   local job=$1 code=$2 output=$3 exec_time=$4
   case $job in
+    \[async])
+      # handle all the errors
+      if (( code == 2 )) || (( code == 3 )) || (( code == 130 )); then
+        prompt_async_inited=0
+        async_stop_worker 'prompt'
+        prompt_async_init
+      fi
+      ;;
     prompt_git_fetch) git_arrow=$output ;;
   esac
   prompt_refresh
@@ -38,16 +46,27 @@ prompt_precmd() {
   async_job 'prompt' prompt_git_fetch
 }
 
+prompt_async_init() {
+  if ((${prompt_async_inited:-0})); then
+    return
+  fi
+  prompt_async_inited=1
+
+  async_init
+  async_start_worker 'prompt' -u -n
+  async_register_callback 'prompt' prompt_callback
+}
+
 prompt_setup() {
   zmodload zsh/zle
   autoload -Uz add-zsh-hook
   add-zsh-hook precmd prompt_precmd
 
-  async_init
-  async_start_worker 'prompt' -u -n
-  async_register_callback 'prompt' prompt_callback
-
+  typeset -g prompt_async_inited
   typeset -g git_arrow
+
+  prompt_async_init
+
   PROMPT='%B%F{cyan}%c%f%b$(git_prompt_info)%B%F{blue}$git_arrow%f%b %(?.%F{green}.%F{red})❯%f '
 
   ZSH_THEME_GIT_PROMPT_PREFIX=" %F{242}"
