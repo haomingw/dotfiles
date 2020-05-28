9f61724011f64dff3d7f0d927c8bc563bf44e520
Haoming Wang <haoming.exe@gmail.com>
2020-05-28 14:39:32 +0200

    shellcheck all bash scripts

--- utils.sh
+++ utils.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ############################  BASIC SETUP TOOLS
 
 msg() {
@@ -23,19 +25,21 @@ file_must_exist() {
 git_clone_to() {
   local git_url=$1
   local target_path=$2
-  local repo_name=$(basename "$git_url" .git)
+
+  local repo_name
+  repo_name=$(basename "$git_url" .git)
 
   if [ -d "$target_path" ] && [ ! -d "$target_path/$repo_name" ]; then
-    cd $target_path && git clone $git_url
+    cd "$target_path" && git clone "$git_url"
   fi
 }
 
 parse_filename() {
-  echo $1 | rev | cut -d'/' -f1 | rev
+  echo "$1" | rev | cut -d'/' -f1 | rev
 }
 
 update_vim_plugins() {
-  if [ -z $CI ]; then
+  if [ -z "$CI" ]; then
     vim +PlugClean! +qall && vim +PlugUpdate +qall
   else
     vim +PlugClean! +qall >/dev/null 2>&1
@@ -48,7 +52,7 @@ update_vim_plugins() {
 do_backup() {
   if [ -e "$1" ]; then
     msg "Attempting to back up your original configuration."
-    today=`date +%Y%m%d_%s`
+    today=$(date +%Y%m%d_%s)
     [ -e "$1" ] && [ ! -L "$1" ] && mv -v "$1" "$1.$today";
     success "Your original configuration has been backed up."
    fi
@@ -57,12 +61,12 @@ do_backup() {
 create_vim_symlinks() {
   local app=$1
 
-  mkdir -p $HOME/.vim/undo
-  for file in $app/vim/vim/*; do
-    lnif $file "$HOME/.vim/$(parse $file)"
+  mkdir -p "$HOME/.vim/undo"
+  for file in "$app"/vim/vim/*; do
+    lnif "$file" "$HOME/.vim/$(parse "$file")"
   done
-  lnif "$app/vim/vimrc"           "$HOME/.vimrc"
-  lnif "$app/vim/ideavimrc"       "$HOME/.ideavimrc"
+  lnif "$app/vim/vimrc"                   "$HOME/.vimrc"
+  lnif "$app/vim/ideavimrc"               "$HOME/.ideavimrc"
 
   lnif "$app/vim/vim/static/clang-format" "$HOME/.clang-format"
   lnif "$app/vim/vim/static/style.yapf"   "$HOME/.style.yapf"
@@ -71,7 +75,7 @@ create_vim_symlinks() {
 }
 
 setup_neovim() {
-  mkdir -p $HOME/.config
+  mkdir -p "$HOME/.config"
   if is_macos; then
     if [ -f /usr/local/bin/pip3 ]; then
       /usr/local/bin/pip3 install -U neovim
@@ -97,13 +101,13 @@ setup_vim_plug() {
 }
 
 use_zsh_plugin() {
-  local pattern="^\s*$1$"
   local target="$HOME/.zshrc"
-  sed -i -e 's/^plugins=(/&\'$'\n'"  $1/" $target
+  # shellcheck disable=SC1003
+  sed -i -e 's/^plugins=(/&\'$'\n'"  $1/" "$target"
 }
 
 clear_zsh_plugins() {
-  sed -i -e '/^plugins/,/^)/{/^plugins/!{/^)/!d;};}' $HOME/.zshrc
+  sed -i -e '/^plugins/,/^)/{/^plugins/!{/^)/!d;};}' "$HOME/.zshrc"
 }
 
 zsh_plug() {
@@ -112,16 +116,17 @@ zsh_plug() {
   local custom_plugins="$ZSH_CUSTOM/plugins"
 
   for plugin in "$@"; do
-    if [[ $plugin == */* ]]; then
-      local name=$(parse $plugin)
-      use_zsh_plugin $name
+    if [[ "$plugin" == */* ]]; then
+      local name
+      name=$(parse "$plugin")
+      use_zsh_plugin "$name"
       if [ ! -d "$custom_plugins/$name" ]; then
-        git_clone_to https://github.com/$plugin.git $custom_plugins
+        git_clone_to https://github.com/"$plugin".git "$custom_plugins"
       else
-        pushd $custom_plugins/$name && git pull && popd
+        pushd "$custom_plugins/$name" && git pull && popd || exit
       fi
     else
-      use_zsh_plugin $plugin
+      use_zsh_plugin "$plugin"
     fi
     msg "Now installing zsh plugin $plugin."
   done
@@ -130,8 +135,8 @@ zsh_plug() {
 append_if_not_exists() {
   local file=$1
   local content=$2
-  if [ ! -f $file ] || ! $(grep -q "$content" $file); then
-    echo $content >> $file
+  if [ ! -f "$file" ] || ! grep -q "$content" "$file"; then
+    echo "$content" >> "$file"
   fi
 }
 
@@ -139,23 +144,25 @@ config_zshrc() {
   local app_path=$1
   local zshrc="$HOME/.zshrc"
 
-  sed -i -e 's/plugins=(git)/plugins=(\'$'\n  git\\'$'\n)/' $zshrc
-  lnif $app_path/common         $HOME/.common
-  for file in $app_path/zsh/*; do
-    lnif $file "$HOME/.$(parse $file)"
+  # shellcheck disable=SC1003
+  sed -i -e 's/plugins=(git)/plugins=(\'$'\n  git\\'$'\n)/' "$zshrc"
+  lnif "$app_path/common" "$HOME/.common"
+  for file in "$app_path"/zsh/*; do
+    lnif "$file" "$HOME/.$(parse "$file")"
   done
 
   local custom_themes="$ZSH_CUSTOM/themes"
-  for file in $app_path/zsh/zsh/themes/*; do
-    lnif $file "$custom_themes/$(basename $file .zsh).zsh-theme"
+  for file in "$app_path"/zsh/zsh/themes/*; do
+    lnif "$file" "$custom_themes/$(basename "$file" .zsh).zsh-theme"
   done
 
+  # shellcheck disable=SC2016
   local cmd='[[ -s $HOME/.zshrc.local ]] && source $HOME/.zshrc.local'
-  append_if_not_exists $zshrc "$cmd"
+  append_if_not_exists "$zshrc" "$cmd"
 
   # set ibus for archlinux
   program_exists pacman && {
-    append_if_not_exists $HOME/.profile "ibus-daemon -drx"
+    append_if_not_exists "$HOME/.profile" "ibus-daemon -drx"
   }
 
   success "Now configuring zsh."
@@ -163,8 +170,8 @@ config_zshrc() {
 
 cleanup_miniconda_files() {
   local installation_file=$1
-  rm $installation_file
-  find $HOME/miniconda3 \( -type f -o -type l \) \
+  rm "$installation_file"
+  find "$HOME/miniconda3" \( -type f -o -type l \) \
     -not -path "$HOME/miniconda3/pkgs*" \
     -regex ".*bin/wish[0-9\.]*$" -ls -delete
   success "Cleaning up minconda files"
@@ -180,48 +187,51 @@ install_miniconda() {
     "yapf"
   )
 
-  if [ ! -d $conda ]; then
+  if [ ! -d "$conda" ]; then
     local url
     local conda_repo="https://repo.anaconda.com/miniconda"
     is_linux && url="$conda_repo/Miniconda3-latest-Linux-x86_64.sh"
     is_macos && url="$conda_repo/Miniconda3-latest-MacOSX-x86_64.sh"
+    # shellcheck disable=SC2236
     if [ ! -z "$url" ]; then
-      local miniconda=$(parse_filename $url)
+      local miniconda
+      miniconda=$(parse_filename $url)
       local target="/tmp"
       [ -f "$target/$miniconda" ] || wget $url -P $target
-      bash $target/$miniconda \
+      bash "$target/$miniconda" \
       && success "Miniconda successfully installed" \
-      && cleanup_miniconda_files $target/$miniconda
+      && cleanup_miniconda_files "$target/$miniconda"
       success "Writing pip packages to $init_pip_packages"
-      $conda/bin/pip freeze > $init_pip_packages
+      "$conda"/bin/pip freeze > "$init_pip_packages"
     fi
   fi
   for package in "${python_packages[@]}"; do
-    $conda/bin/pip install -U $package
+    "$conda"/bin/pip install -U "$package"
   done
 }
 
 install_cargo() {
   local cargo="$HOME/.cargo"
-  if [[ ! -d $cargo ]]; then
+  if [[ ! -d "$cargo" ]]; then
     local target='/tmp/install-rust.sh'
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $target
-    sh $target -y --no-modify-path
-    rm $target
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > "$target"
+    sh "$target" -y --no-modify-path
+    rm "$target"
   fi
   local packages=(
     "fd-find"
     "ripgrep"
   )
   for package in "${packages[@]}"; do
-    $cargo/bin/cargo install $package
+    "$cargo"/bin/cargo install "$package"
   done
 }
 
 watch_limit_is_increased() {
   program_exists sysctl && {
-    local limit=$(sysctl fs.inotify.max_user_watches | cut -d' ' -f3)
-    [ $limit -ge 524288 ]
+    local limit
+    limit=$(sysctl fs.inotify.max_user_watches | cut -d' ' -f3)
+    [ "$limit" -ge 524288 ]
   }
 }
 
@@ -237,14 +247,16 @@ increase_watch_limit() {
     "Do you want to increase (requires sudo password)"
     "inotify limit? (y/N) "
   )
-  if [ ! -z $cfg ] && [ -r $cfg ]; then
+  # shellcheck disable=SC2236
+  if [ ! -z "$cfg" ] && [ -r "$cfg" ]; then
+    # shellcheck disable=SC2162
     read -p "${message[*]}"
     case $REPLY in
       [yY][eE][sS]|[yY]) ;;
       *) return 0 ;;
     esac
-    cat $cfg | grep -q "$watch_limit" || {
-      echo $watch_limit | sudo tee -a $cfg >/dev/null
+    grep -q "$watch_limit" "$cfg" || {
+      echo "$watch_limit" | sudo tee -a "$cfg" >/dev/null
     }
     program_exists apt && sudo sysctl -p >/dev/null
     program_exists pacman && sudo sysctl --system >/dev/null
@@ -260,7 +272,7 @@ install_vscode_extensions() {
     "mitaki28.vscode-clang"
   )
   for extension in "${extensions[@]}"; do
-    code --install-extension $extension
+    code --install-extension "$extension"
   done
   success "Vscode extensions are installed"
 }
