d5a988053a2c58fd8dbae7cb7fc4560e3775023a
Haoming Wang <haoming.exe@gmail.com>
2020-09-04 14:05:32 +0200

    workaround of lf ui issue

--- utils.sh
+++ utils.sh
@@ -321,6 +321,7 @@ install_go_tools() {
 
     local lfrc="$HOME/.config/lf"
     safe_mkdir "$lfrc"
+    safe_mkdir "$HOME/.trash"
     lnif "$app_path/lf/lfrc" "$lfrc"
 
     success "Now configuring lf."
