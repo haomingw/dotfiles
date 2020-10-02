c73b6192536fe71c1ac3eee1041f6156b3fd1bf0
Haoming Wang <haoming.exe@gmail.com>
2020-10-02 17:21:32 +0200

    check apt repo existence before adding

--- utils.sh
+++ utils.sh
@@ -407,12 +407,14 @@ install_docker() {
       gnupg-agent \
       software-properties-common
 
-    # Add Docker’s official GPG key
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-     $(lsb_release -cs) \
-     stable"
+    if grep -q docker /etc/apt/sources.list; then
+      # Add Docker’s official GPG key
+      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+      sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+    fi
 
     sudo apt update
     sudo apt install -y docker-ce docker-ce-cli containerd.io
