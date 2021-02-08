c75309ac28de3ab3148d9f2898a67adeac12374e
Haoming Wang <haoming.exe@gmail.com>
2021-02-08 10:26:33 +0100

    fix llvm default branch

--- mirror.sh
+++ mirror.sh
@@ -48,7 +48,7 @@ zsh_aync="zsh/zsh/async.zsh"
 
 declare -A files=(
   ["$clang_format"]="https://raw.githubusercontent.com/llvm/llvm-project/\
-master/clang/tools/clang-format/clang-format.py"
+main/clang/tools/clang-format/clang-format.py"
   ["$tldr_sh"]="https://raw.githubusercontent.com/raylee/tldr/master/tldr"
   ["$zsh_aync"]="https://raw.githubusercontent.com/mafredri/zsh-async/master/async.zsh"
 )
