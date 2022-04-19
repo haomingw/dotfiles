a3f45eff08a57911d015b17dde039296e711c1c5
Haoming Wang <haoming.exe@gmail.com>
2022-04-19 14:30:00 +0200

    switch left and right option for weztern

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,6 +7,7 @@ return {
   font_size = 20,
   harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
   default_cursor_style = "BlinkingUnderline",
+  send_composed_key_when_left_alt_is_pressed = true,
   send_composed_key_when_right_alt_is_pressed = false,
   keys = {
     {key="LeftArrow", mods="CMD", action=wezterm.action{ActivateTabRelative=-1}},
