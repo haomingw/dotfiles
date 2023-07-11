2ae8acac6ae4d829bba748f09b2de45fc7f5b054
Haoming Wang <haoming.exe@gmail.com>
2023-07-11 17:26:56 +0200

    [nvim] revert color config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -100,10 +100,12 @@ require("lazy").setup({
   },
   {
       "ellisonleao/gruvbox.nvim",
+      priority = 1000,
       config = function()
         require("gruvbox").setup({
           transparent_mode = true,
         })
+        vim.cmd("colorscheme gruvbox")
       end
   },
   -- Completion
