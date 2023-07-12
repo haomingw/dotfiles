b4561ad9558bed79bbffba9f2d4da64c1509e078
Haoming Wang <haoming.exe@gmail.com>
2023-07-12 14:41:58 +0200

    add back indent object

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
   "romainl/vim-cool",
   "rhysd/clever-f.vim",
   "haomingw/vim-startscreen",
+  "michaeljsmith/vim-indent-object",
   {
     "kana/vim-textobj-user",
     config = function()
