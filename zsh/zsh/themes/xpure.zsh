ab289d8adeaf86bfa2056b2d12b316fb16a1e5bd
Haoming Wang <haoming.exe@gmail.com>
2026-04-15 01:33:41 +0200

    minimalist zsh prompt

--- zsh/zsh/themes/xpure.zsh
+++ zsh/zsh/themes/xpure.zsh
@@ -1,97 +1,21 @@
-source $HOME/.zsh/async.zsh
+git_arrows() {
+  # Only run inside a git repo
+  git rev-parse --is-inside-work-tree &>/dev/null || return
 
-git_has_upstream() {
-  command git rev-parse --abbrev-ref "@{upstream}" &>/dev/null
-}
+  # Get ahead/behind counts
+  local ahead behind
+  read ahead behind <<< $(git rev-list --left-right --count HEAD..."@{upstream}" 2>/dev/null)
 
-update_git_arrow() {
-  local arrows left=${1:-0} right=${2:-0}
+  local arrows
 
-  (( right > 0 )) && arrows+=⇣
-  (( left > 0 )) && arrows+=⇡
+  (( ahead > 0 )) && arrows+="⇡"
+  (( behind > 0 )) && arrows+="⇣"
 
-  reply=$arrows
-}
-
-prompt_update_git_arrow() {
-  local output=$(command git rev-list --left-right --count HEAD..."@{upstream}" 2>/dev/null)
-  local tab=$(printf '\t')
-  [[ $output == *$tab* ]] && update_git_arrow ${(ps:\t:)output} || reply=
-}
-
-arrow_prompt_info() {
-  git_has_upstream && {
-    prompt_update_git_arrow
-    prompt_git_arrows=$reply
-    [[ -n $prompt_git_arrows ]] && echo " $prompt_git_arrows"
-  }
-}
-
-prompt_git_fetch() {
-  git_has_upstream && {
-    local ref=$(command git symbolic-ref -q HEAD)
-    local remote=($(command git for-each-ref --format='%(upstream:remotename) %(refname)' $ref))
-    # No remote specified for this branch, skip fetch
-    [[ -z $remote[1] ]] && return 97
-    command git fetch $remote
-  }
-}
-
-prompt_refresh() {
-  zle && zle .reset-prompt
-}
-
-prompt_callback() {
-  local job=$1 code=$2 output=$3 exec_time=$4
-  case $job in
-    \[async])
-      # handle all the errors
-      if (( code == 2 )) || (( code == 3 )) || (( code == 130 )); then
-        typeset -g prompt_async_inited=0
-        async_stop_worker 'prompt'
-        prompt_async_init # reinit the worker.
-      fi
-      ;;
-    prompt_git_fetch)
-      if (( code == 0 )); then
-        prompt_update_git_arrow
-        [[ $prompt_git_arrows != $reply ]] && prompt_refresh
-      fi
-      ;;
-  esac
-}
-
-prompt_precmd() {
-  local host="$(hostname)"
-  print -Pn '\e]0;${host:0:8}:%~\a'
-  async_worker_eval 'prompt' builtin cd -q $PWD
-  async_job 'prompt' prompt_git_fetch
-}
-
-prompt_async_init() {
-  typeset -g prompt_async_inited
-  if ((${prompt_async_inited:-0})); then
-    return
-  fi
-  prompt_async_inited=1
-
-  async_init
-  async_start_worker 'prompt' -u -n
-  async_register_callback 'prompt' prompt_callback
+  [[ -n "$arrows" ]] && echo " $arrows"
 }
 
 prompt_setup() {
-  # zmodload zsh/zle
-  # autoload -Uz add-zsh-hook
-  # add-zsh-hook precmd prompt_precmd
-
-  # typeset -g prompt_git_arrows
-  # typeset -g reply
-
-  # prompt_async_init
-
-  # PROMPT='%B%F{cyan}%c%f%b$(git_prompt_info)%B%F{blue}$(arrow_prompt_info)%f%b %(?.%F{green}.%F{red})❯%f '
-  PROMPT='%B%F{cyan}%c%f%b$(git_prompt_info) %(?.%F{green}.%F{red})❯%f '
+  PROMPT='%B%F{cyan}%c%f%b$(git_prompt_info)%B%F{blue}$(git_arrows)%f%b %(?.%F{green}.%F{red})❯%f '
 
   ZSH_THEME_GIT_PROMPT_PREFIX=" %F{magenta}"
   ZSH_THEME_GIT_PROMPT_SUFFIX="%f"
